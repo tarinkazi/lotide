2590419223e4367700deee5e5327a4e546a29456
Tarin Kazi <tarin.kazi.afrin@gmail.com>
2021-10-25 21:07:08 -0400

    upload

---
+++ assertEqual.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    let str = "✅Assertion Passed: " + actual + "===" + expected;
+     
+    return str;
+  } else {
+    let str = "🛑🛑Assertion Failed :   " + actual + "!=" + expected;
+    return str;
+  }
+
+
+};
+
+// TEST CODE
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+//console.assert(str);
+console.log(assertEqual(1, 1));
