ee0c234406ea31b8da23102e5a2252c35c854536
Tarin Kazi <tarin.kazi.afrin@gmail.com>
2021-10-28 02:10:43 -0400

    update

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+const  assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    //let str = "✅Assertion Passed: " + actual + "===" + expected;
+    let str = `✅Assertion Passed:, ${actual}, === , ${expected}`;
+     
+    return str;
+  } else {
+    let str = `🛑🛑Assertion Failed:, ${actual}, === , ${expected}`;
+    return str;
+  }
+
+
+};
+
+const findKeyByValue = function(obj, valToFind) {
+  for(let objKey in obj) {
+    if(obj[objKey] === valToFind){
+      return objKey;
+    }
+  }
+}
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama"));
+console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined));
