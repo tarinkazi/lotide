650208eae98801ae7f33816747c48d51ad8a6747
Tarin Kazi <tarin.kazi.afrin@gmail.com>
2021-11-01 18:06:50 -0400

    middle up

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,47 @@
-//const assertArraysEqual = require('../assertArraysEqual');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-assertArraysEqual(middle([1, 2],[]));// => []
-assertArraysEqual(middle([1, 2, 3]),[2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]); // => [3]
-assertArraysEqual(middle([1, 2, 3, 4]),[2, 3]);// => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3, 4]) // => [3, 4]
+
+
+describe("#middle", () => {
+  it("returns [2] for [1, 2, 3]", () => {
+    
+
+    assert.deepEqual(middle([1, 2, 3]),2);
+  });
+  it("returns '[]' for ['5']", () => {
+    assert.deepEqual(middle(['5']), []); 
+  });
+  it("returns '[]' for ['Lighthouse Labs', 'Bootcamp']", () => {
+    assert.deepEqual(middle(['Lighthouse Labs', 'Bootcamp']), []); 
+  });
+  it("returns '[]' for ['1',2]", () => {
+    assert.deepEqual(middle(['1',2]), []); 
+  });
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), 3);
+  });
+  it("returns '[Labs]' for ['Lighthouse Labs', 'Bootcamp']", () => {
+    assert.deepEqual(middle(['Lighthouse', 'Labs', 'Bootcamp']), 'Labs'); 
+  });
+ 
+});
+
+
+
+
+
+
+//const assertArraysEqual = require('../assertArraysEqual');
+//const assertArraysEqual = require('../assertArraysEqual');
+//const middle = require('../middle');
+
+// assertArraysEqual(middle([1, 2],[]));// => []
+// assertArraysEqual(middle([1, 2, 3]),[2]); // => [2]
+// assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]); // => [3]
+// assertArraysEqual(middle([1, 2, 3, 4]),[2, 3]);// => [2, 3]
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3, 4]) // => [3, 4]
