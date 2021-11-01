56191662db7ee526bc9f7f206a49728671ec5a22
Tarin Kazi <tarin.kazi.afrin@gmail.com>
2021-11-01 14:56:22 -0400

    update3

--- eqArrays.js
+++ eqArrays.js
@@ -1,16 +1,5 @@
-const  assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    //let str = "✅Assertion Passed: " + actual + "===" + expected;
-    let str = `✅Assertion Passed:, ${actual}, === , ${expected}`;
-     
-    return str;
-  } else {
-    let str = `🛑🛑Assertion Failed:, ${actual}, === , ${expected}`;
-    return str;
-  }
+const assertEqual = require ('./assertEqual')
 
-
-};
 
 const eqArrays = function(arr1, arr2) {
   let flag = 0;
@@ -20,21 +9,13 @@ const eqArrays = function(arr1, arr2) {
         
     } else if (arr1[i] === arr2[i]) {
         flag = 1;
-        //console.log(arr2[])
+        //assertEqual(arr2[])
     }
   }
   if (flag === 1) {
     return true;
   }
 }
- 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1]));// => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));// => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), true)); 
+module.exports = eqArrays;
 
 
-//const name = 'Alice';
-//.log(`Hello, ${name}!`); // logs
